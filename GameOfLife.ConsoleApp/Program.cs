0885e4568236e282bdfb632179b9084a51fc19d9
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-09-28 23:38:42 +0900

    メイン処理を追加

--- GameOfLife.ConsoleApp/Program.cs
+++ GameOfLife.ConsoleApp/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using GameOfLife.Lib;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
 
-namespace GameOfLife.ConsoleApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace GameOfLife.ConsoleApp {
+  class Program {
+    static void Main(string[] args) {
+      var rule = new Rule();
+      var builder = new CellBuilder(rule);
+      var cells = builder.BuildCells(
+        rowLength: 20,
+        colLength: 20,
+        percentOfLivingCells: 30);
+      var grid = new Grid(cells);
+      var writer = new StreamWriter(Console.OpenStandardOutput(), Encoding.ASCII);
+      var drawer = new ConsoleDrawer(writer);
+      while (!Console.KeyAvailable) {
+        drawer.DrawCells(grid.Cells);
+        grid.ToNextGen();
+        Thread.Sleep(200);
+      }
     }
+  }
 }
