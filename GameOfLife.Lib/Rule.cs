5db6d8641fbbb3ff2268d80706ab55e28e49023a
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-09-28 23:30:48 +0900

    ルールを実装

--- GameOfLife.Lib/Rule.cs
+++ GameOfLife.Lib/Rule.cs
@@ -1,8 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace GameOfLife.Lib {
   public class Rule {
-    internal Cell.State GetNextState(Cell.State self, IList<Cell> neighbours) => throw new NotImplementedException();
+    // 周囲の生きているセルの数から次代の生死を判定する
+    internal Cell.State GetNextState(Cell.State self, IList<Cell> neighbours) {
+      int alives = neighbours.Count(cell => cell.Current == Cell.State.Alive);
+      bool willBorn = alives == 3;
+      bool isSurvive = alives == 2 && self == Cell.State.Alive;
+      return (willBorn || isSurvive) ? Cell.State.Alive : Cell.State.Dead;
+    }
   }
 }
