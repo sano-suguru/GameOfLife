678f51a1bc730971ba75e422e85014868942e1a2
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-09-28 23:29:41 +0900

    セルとルールの仮実装を追加

---
+++ GameOfLife.Lib/Cell.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace GameOfLife.Lib {
+  public class Cell {
+    // 生死
+    public enum State { Alive, Dead }
+
+    // 現在の生死
+    internal State Current { get; private set; }
+
+    // 次の世代の生死
+    State Next { get; set; }
+
+    // 周囲のセル
+    IList<Cell> Neighbours { get; }
+
+    // 次の世代の生死を判定するルール
+    Rule Rule { get; }
+
+    // 初期状態を受け取る。ルールはDIします。
+    public Cell(State state, Rule rule) {
+      Current = state;
+      Rule = rule;
+      Neighbours = new List<Cell>();
+    }
+
+    // 次の世代の生死を設定します。
+    internal void SetNextState() =>
+        Next = Rule.GetNextState(self: Current, Neighbours);
+
+    // 世代を進めます。
+    internal void ToNextGen() => Current = Next;
+
+    // 周囲のセルを設定します。
+    internal void AddNeighbour(Cell neighbour) => Neighbours.Add(neighbour);
+
+    // コンソール出力時の描画
+    public override string ToString() => Current == State.Alive ? "o " : ". ";
+  }
+}
---
+++ GameOfLife.Lib/Rule.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfLife.Lib {
+  public class Rule {
+    internal Cell.State GetNextState(Cell.State self, IList<Cell> neighbours) => throw new NotImplementedException();
+  }
+}
